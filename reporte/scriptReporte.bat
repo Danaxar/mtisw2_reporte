823a72521485f31b7d593126a0d3bfe629e4f684
Daniel <daniel.catalan@usach.cl>
2022-11-11 20:38:46 -0300

    avance

--- reporte/scriptReporte.bat
+++ reporte/scriptReporte.bat
@@ -1,6 +1,6 @@
 git add *
 git commit -m "avance"
 git push
-mvnw clean install -DskipTests
+./mvnw clean install -DskipTests
 docker build -t danaxar/reporte-microservice .
 docker push danaxar/reporte-microservice
