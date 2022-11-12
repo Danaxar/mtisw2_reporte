cfb1d08d9e5f3b9450d0e0b3bf9ea52963904c9a
Daniel <daniel.catalan@usach.cl>
2022-11-11 21:06:08 -0300

    avance

--- reporte/src/main/java/com/example/reporte/ReporteApplication.java
+++ reporte/src/main/java/com/example/reporte/ReporteApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReporteApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Running.");
 		SpringApplication.run(ReporteApplication.class, args);
 	}
 
